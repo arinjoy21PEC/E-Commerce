0b0c88a0d15086ed2789ce7cbe88d3c48f892247
arinjoy21PEC <114183787+arinjoy21PEC@users.noreply.github.com>
2023-07-23 19:59:50 +0530

    Update middlewares.js

--- api/config/middlewares.js
+++ api/config/middlewares.js
@@ -6,7 +6,7 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'http://localhost:1337/admin'],
+          'connect-src': ["'self'"],
           'img-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
           'media-src': ["'self'", 'data:', 'blob:', 'res.cloudinary.com'],
           upgradeInsecureRequests: null,

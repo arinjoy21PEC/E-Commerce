427f171ba26fea4aec901d4ed2600bbc5690db8b
arinjoy21PEC <114183787+arinjoy21PEC@users.noreply.github.com>
2023-07-24 16:11:36 +0530

    Update card.jsx

--- client/src/components/card/card.jsx
+++ client/src/components/card/card.jsx
@@ -4,16 +4,16 @@ import { Link } from "react-router-dom";
 
 
 const card = ({item}) => {
-  console.log(process.env.REACT_APP_UPLOAD_URL + item.attributes?.img.data.attributes.url);
-  console.log(process.env.REACT_APP_UPLOAD_URL + item.attributes?.img2.data.attributes.url);
+  console.log(item.attributes?.img.data.attributes.url);
+  console.log(item.attributes?.img2.data.attributes.url);
 
   return (
     <Link className="link" to={`/product/${item.id}`}>
     <div className='card'>
       <div className='image'>
         {item?.attributes.isNew && <span>New Season</span>}
-        <img src={process.env.REACT_APP_UPLOAD_URL+item.attributes?.img.data.attributes.url} alt="" className='mainImg' />
-        <img src={process.env.REACT_APP_UPLOAD_URL+item.attributes?.img2.data.attributes.url} alt="" className='secondImg' />
+        <img src={item.attributes?.img.data.attributes.url} alt="" className='mainImg' />
+        <img src={item.attributes?.img2.data.attributes.url} alt="" className='secondImg' />
       </div>
       <h2>{item?.attributes.title}</h2>
       <div className='prices'>
